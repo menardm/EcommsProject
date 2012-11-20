cbb78abe96fa9e9fa94f2d6f5114d925fc0f2429
Matthew Menard <menard-m@hotmail.com>
2012-11-20 13:00:55 -0600

    initial upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erer::Application.config.secret_token = 'd5877c4fe591af2eda53875d74226b273a297e1556a05e61a46253340c6aec552c4d68e175e6ecd45f4397c50f51cc009cc219e7756b3a610ab9c656aea0d2e4'
